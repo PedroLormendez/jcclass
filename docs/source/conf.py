dd101b461a9325fe3011491c4dbfcb1406837f1f
Pedro Herrera Lormendez <Pedro.Herrera-Lormendez@ioez.tu-freiberg.de>
2022-08-17 22:48:35 +0200

    updated confy.py theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,21 +1,25 @@
 # Configuration file for the Sphinx documentation builder.
 
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
 # -- Project information
 
-import sys 
+
+import sys
 import os
 
 sys.path.insert(0, os.path.abspath('../..'))
 sys.path.insert(0, os.path.abspath('../../jcclass'))
 
 vfile = {}
-exec(open('../../jcclass/version,py').read(), vfile)
+exec(open('../../jcclass/version.py').read(), vfile)
 
-project = 'JCclass'
-copyright = '2022, Pedro'
+project = 'jcclass'
+copyright = '2022, Pedro Herrera-Lormendez'
 author = 'Pedro Herrera Lormendez'
 
-release = vfile['__version']
+release = vfile['__version__']
 
 # -- General configuration
 
@@ -26,6 +30,7 @@ extensions = [
     'sphinx.ext.autosummary',
     'sphinx.ext.intersphinx',
 ]
+
 
 intersphinx_mapping = {
     'python': ('https://docs.python.org/3/', None),
@@ -38,6 +43,22 @@ templates_path = ['_templates']
 # -- Options for HTML output
 
 html_theme = 'sphinx_rtd_theme'
+html_theme_options = {
+
+    # Set the name of the project to appear in the navigation.
+    'nav_title': 'JCclass',
+
+    # Set you GA account ID to enable tracking
+    #'google_analytics_account': 'XXX',
+
+    # Specify a base_url used to generate sitemap.xml. If not
+    # specified, then no sitemap will be built.
+    'base_url': 'https://github.com/PedroLormendez/jcclass',
+    # Set the repo location to get a badge with stats
+    'repo_url': 'https://github.com/PedroLormendez/jcclass',
+    'repo_name': 'JCclass',
+    'repo_type': 'github',
+    }
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
